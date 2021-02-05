9037c301b5dbbfb3ebf819d47c96217a4121f60d
Prashant Sedhain <49375658+PrashantSedhain@users.noreply.github.com>
2021-02-04 18:51:21 -0600

    Homepage finalized

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,24 +1,23 @@
 import React from "react";
 import styles from "./About.css";
-
 const About = () => {
   return (
     <div>
       <div className="row">
-        <div className="col-sm">
+        <div className={["col-sm", `${styles.imageStyle}`].join(" ")}>
           <img
             style={{
-              height: "90%",
-              width: "90%",
-              borderRadius: "50%",
+              height: "95%",
+              width: "95%",
+              borderRadius: "40%",
               margin: 10,
             }}
             src="https://images.unsplash.com/photo-1609269623809-957fe18fe113?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
             alt="Girl in a jacket"
-          />{" "}
+          />
         </div>
         <div className="col-sm">
-          <p style={{ margin: 30, fontWeight: "lighter", fontSize: 22 }}>
+          <p style={{ fontWeight: "lighter", fontSize: 22, margin: 10 }}>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             Repellendus ab nulla dolorum autem nisi officiis blanditiis
             voluptatem hic, assumenda aspernatur facere ipsam nemo ratione
@@ -29,9 +28,10 @@ const About = () => {
           <p
             style={{
               marginTop: 5,
-              marginLeft: 30,
-              fontWeight: "lighter",
+              marginLeft: 10,
+              fontWeight: "bold",
               fontSize: 22,
+              textAlign: "justify",
             }}
           >
             -Elizabeth-
